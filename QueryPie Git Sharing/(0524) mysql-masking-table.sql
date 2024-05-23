9a37a750893c8789743b548ce42a4ed5d2bbe852
Daniel Son <daniel@chequer.io>
2024-05-23 06:14:47 +0000

    Update file 'QueryPie Git Sharing/(0524) mysql-masking-table.sql'

--- QueryPie Git Sharing/(0524) mysql-masking-table.sql
+++ QueryPie Git Sharing/(0524) mysql-masking-table.sql
@@ -1,6 +1,8 @@
 USE sakila;
+UPDATE actor SET first_name = 'ella' WHERE actor_id = 1;
+
 SELECT * FROM customer;
 SELECT * FROM cs_memo;
 
-UPDATE actor SET first_name = 'ella' WHERE actor_id = 1;
+
 
